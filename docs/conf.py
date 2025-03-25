2eb8fd21aa35cc60e2af1d6f45ed569b4471b2e4
Andrea Cossa <andrea.cossa05@gmail.com>
2025-03-25 13:17:17 +0100

    add docs requirements

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
-#
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
@@ -36,8 +36,7 @@ napoleon_use_ivar = True
 autodoc_docstring_signature = False
 
 # -- Options for HTML output -------------------------------------------------
-html_static_path = ['_static']
-templates_path = ['_templates']
 exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
+# templates_path = ['_templates']

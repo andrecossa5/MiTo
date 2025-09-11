352c199c9ba75b89b5ccdb61ff6baf336478f5e2
Andrea Cossa <andrea.cossa05@gmail.com>
2025-09-11 20:20:42 +0200

    Bump version to 0.0.3 and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.0.3] - 2025-09-11
+### Added
+- Code refactoring and improvements
+- Enhanced functionality and bug fixes
+- Updated documentation
+
 ## [0.0.2] - 2025-03-25
 ### Added
 - Updated docs.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mito-utils",
-    version="0.0.2",
+    version="0.0.3",
     author="Your Name",
     author_email="your.email@example.com",
     description="Utilities for MT-based single cell Lineage Tracing (scLT).",
